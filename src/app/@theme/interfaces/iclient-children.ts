701e741d46af1cf3bd4d564c3c83691336837fba
Jean <jeandmalves@gmail.com>
2021-11-25 15:10:54 -0300

    Adicionado o serviço storage e melhorado o CSS

---
+++ src/app/@theme/interfaces/iclient-children.ts
@@ -0,0 +1,6 @@
+export interface IClientChildren {
+    id:number,
+    name:string,
+    chores:string[],
+    allowance: number
+}
--- src/app/@theme/theme.module.ts
+++ src/app/@theme/theme.module.ts
@@ -4,12 +4,14 @@ import { LoginScreenComponent } from './components/login-screen/login-screen.com
 import { MaterialExampleModule } from '../../material.module';
 import { ReactiveFormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
     imports: [
         MaterialExampleModule,
         ReactiveFormsModule,
         CommonModule,
+        HttpClientModule
     ],
 	declarations: [
         LoginBoxComponent,
