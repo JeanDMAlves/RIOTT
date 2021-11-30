0cd033c377f6c58647e0b1646fc695470e01908b
Jean <jeandmalves@gmail.com>
2021-11-30 16:27:21 -0300

    Rotas de Login e Logged feitas, Header pronto

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -2,7 +2,7 @@ import { LoginScreenComponent } from './login-screen/login-screen.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginBoxComponent } from './login-box/login-box.component';
-import {MaterialExampleModule} from '../../../material.module';
+import { MaterialExampleModule } from '../../../material.module';
 import { ReactiveFormsModule } from "@angular/forms";
 
 
