f55ab3d82f2dbff1ab2712de7ac46a6f835205d2
Lucas Alexander <lucasalexander13@hotmail.com>
2021-12-02 19:16:23 -0300

    docs: documentação dos métodos - LoginBoxComponent

--- src/app/pages/login/login-box/login-box.component.ts
+++ src/app/pages/login/login-box/login-box.component.ts
@@ -11,24 +11,40 @@ export class LoginBoxComponent{
   public hide: boolean = true;
   public email: FormControl = new FormControl('', [Validators.required, Validators.email])
   public password: FormControl = new FormControl('', [Validators.required])
-
+  /**
+   * Checa as condições do campo e mostra uma mensagem ao usuário caso
+   * tenha inserido um email inválido ou tenha deixado o campo vazio.
+   * 
+   * @returns mensagem de alerta ou vazia
+   */
   public getEmailErrorMessage(): string {
     if (this.email.hasError('required')) {
       return 'Este campo é obrigatório';
     }
     return this.email.hasError('email') ? 'Digite um email válido' : '';
   }
-  public getPasswordErrorMessage(): any {
+  /**
+   * Checa as condições do campo e mostra uma mensagem ao usuário caso
+   * tenha inserido uma senha muito curta ou tenha deixado o campo vazio.
+   * 
+   * @returns mensagem de alerta ou vazia
+   */
+  public getPasswordErrorMessage(): string {
     if (this.password.hasError('required')) {
       return 'Este campo é obrigatório';
     }
     return this.password.hasError('minlength') ? 'Senha muito curta' : '';
   }
-
-  private getClientData(): IClient{
-    return {email:this.email.value, password:this.password.value};
+  /**
+   * Recebe os valores de input do usuário e retorna uma interface
+   * do tipo ICliente armazenando os valores 
+   */
+  private getClientData(): IClient {
+    return {email: this.email.value, password: this.password.value};
   }
-
+  /**
+   * Ao enviar o formulário, imprime os valores inseridos no console
+   */
   onSubmit(): void{
     console.log(this.getClientData());
   }
