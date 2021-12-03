f2ee0e8a6874dd3d731a7d095a479a117f4aca62
Lucas Alexander <lucasalexander13@hotmail.com>
2021-12-03 20:34:36 -0300

    style: estilização do navigation-header

--- src/app/pages/homePage/navegation-header/navegation-header.component.ts
+++ src/app/pages/homePage/navegation-header/navegation-header.component.ts
@@ -1,9 +1,10 @@
-import { Component} from '@angular/core';
+import { Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-navegation-header',
   templateUrl: './navegation-header.component.html',
-  styleUrls: ['./navegation-header.component.css']
+  styleUrls: ['./navegation-header.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class NavegationHeaderComponent{
   public teste:number;
