fd682c153262e32ab571264733dc20c384895221
Lucas Alexander <lucasalexander13@hotmail.com>
2021-12-06 20:01:08 -0300

    fix: correção na estilização do navigation-header

--- src/app/pages/homePage/navegation-header/navegation-header.component.ts
+++ src/app/pages/homePage/navegation-header/navegation-header.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-navegation-header',
   templateUrl: './navegation-header.component.html',
   styleUrls: ['./navegation-header.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class NavegationHeaderComponent{
   @Input() tabLinks: any[] = [];
