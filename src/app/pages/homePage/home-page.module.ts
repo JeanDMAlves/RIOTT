2f044d80ddbb088642e79ff1e08441ed468e9a1f
Lucas Alexander <lucasalexander13@hotmail.com>
2021-12-14 21:53:42 -0300

    fix: atualização dos arquivos

--- src/app/pages/homePage/home-page.module.ts
+++ src/app/pages/homePage/home-page.module.ts
@@ -1,14 +1,9 @@
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
-import { HttpClientModule } from '@angular/common/http';
-import { NavegationHeaderComponent } from './navegation-header/navegation-header.component';
-import { ListsComponent } from './lists/lists.component';
-import { HomePageComponent } from './home-page.component';
-import { ListMemberItemComponent } from './lists/list-member-item/list-member-item.component';
-import { ManageButtonComponent } from './lists/manage-button/manage-button.component';
-import { ListMemberActivitiesComponent } from './lists/list-member-activities/list-member-activities.component';
-import { ListMemberAbsencesComponent } from './lists/list-member-absences/list-member-absences.component';
+import { HttpClientModule } from "@angular/common/http";
+import { NavegationHeaderComponent } from "./navegation-header/navegation-header.component";
+import { HomePageComponent } from "./home-page.component";
 import { HomePageRoutingModule } from "./home-page-routing.module";
 import { ThemeModule } from "src/app/@theme/theme.module";
 import { RouterModule } from "@angular/router";
@@ -19,30 +14,21 @@ import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { MatCardModule } from "@angular/material/card";
 
-
 @NgModule({
-  declarations: [
-    NavegationHeaderComponent,
-    ListsComponent,
-    HomePageComponent,
-    ListMemberItemComponent,
-    ManageButtonComponent,
-    ListMemberActivitiesComponent,
-    ListMemberAbsencesComponent,
-  ],
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    HomePageRoutingModule,
-    ThemeModule,
-    RouterModule,
-    AppRoutingModule,
-    MatTabsModule,
-    MatSidenavModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatCardModule,
-  ],
+    declarations: [NavegationHeaderComponent, HomePageComponent],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        HomePageRoutingModule,
+        ThemeModule,
+        RouterModule,
+        AppRoutingModule,
+        MatTabsModule,
+        MatSidenavModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatCardModule,
+    ],
 })
-export class HomePageModule { }
+export class HomePageModule {}
