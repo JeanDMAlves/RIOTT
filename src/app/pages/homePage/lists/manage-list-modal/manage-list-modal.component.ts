64551c97c1d538534d5d2948003d35eb8bdcdbe0
jeandmalves <jeandmalves@gmail.com>
2022-01-05 18:50:24 -0300

    feat: Implementado modal de exclusão da TaskList

--- src/app/pages/homePage/lists/manage-list-modal/manage-list-modal.component.ts
+++ src/app/pages/homePage/lists/manage-list-modal/manage-list-modal.component.ts
@@ -1,3 +1,5 @@
+import { ModalComponent } from "src/app/@theme/components/modal/modal.component";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 import { ActivityListService } from "./../../../../@core/services/activity-list.service";
 import { ActivityService } from "./../../../../@core/services/activity.service";
 import { IData } from "./../../../../@theme/interfaces/IData";
@@ -5,6 +7,7 @@ import { IMember } from "./../../../../@theme/interfaces/IMember";
 import { MemberService } from "./../../../../@core/services/member.service";
 import { Component, OnInit, TemplateRef, ViewChild } from "@angular/core";
 import { ITaskList } from "src/app/@theme/interfaces/ITaskList";
+import { IResultOperation } from "src/app/@theme/interfaces/IResultOperation";
 
 @Component({
     selector: "app-manage-list-modal",
@@ -14,14 +17,72 @@ import { ITaskList } from "src/app/@theme/interfaces/ITaskList";
 export class ManageListModalComponent implements OnInit {
     @ViewChild("rawTasks") rawTasksTemplate: TemplateRef<any>;
     @ViewChild("memberSelectedTaskList") memberSelectedTaskList: TemplateRef<any>;
+    @ViewChild("modalDeleteTask") modalDeleteTask: TemplateRef<any>;
+    @ViewChild("modalDeleteTaskSuccessful") modalDeleteTaskSuccessful: TemplateRef<any>;
 
     public shownTemplate: TemplateRef<any>;
     public members: IMember[] = [];
     public tasks: IData[] = [];
-    public selectedMemberTasks: ITaskList[];
+    public selectedMemberTasks: ITaskList;
     private selectedMemberId: string;
 
-    constructor(private memberService: MemberService, private activityService: ActivityService, private taskListService: ActivityListService) {}
+    constructor(
+        private memberService: MemberService,
+        private activityService: ActivityService,
+        private taskListService: ActivityListService,
+        private modalService: NgbModal,
+    ) {}
+
+    public modalDeleteTaskData: IResultOperation = {
+        img: "../../../../assets/Warning.png",
+        message: "Deseja realmente excluir essa lista?",
+        buttons: [
+            {
+                label: "Confirmar e excluir a lista",
+                class: "dark-green-button modalResultOperationButton",
+                action: (): void => {
+                    this.taskListService.deleteTaskListById(this.selectedMemberTasks.id).subscribe(() => {
+                        this.modalService.dismissAll();
+                        this.openModals(this.modalDeleteTaskSuccessful, "");
+                    });
+                    console.log(this.selectedMemberTasks);
+                },
+            },
+            {
+                label: "Cancelar",
+                class: "orange-button modalResultOperationButton",
+                action: (): void => {
+                    this.modalService.dismissAll();
+                },
+            },
+        ],
+    };
+
+    public modalDeleteTaskSuccessfulData: IResultOperation = {
+        img: "../../../../assets/Successful.png",
+        message: "Lista excluída com sucesso!",
+        buttons: [
+            {
+                label: "Finalizar",
+                class: "dark-green-button modalResultOperationButton",
+                action: (): void => {
+                    this.modalService.dismissAll();
+                },
+            },
+        ],
+    };
+
+    /**
+     * Abre um modal
+     * @param modalBody - Corpo do modal
+     * @param title - Título que será exibido
+     * na parte esquerda do header do modal
+     */
+    private openModals(modalBody: TemplateRef<any>, title?: string): void {
+        const openedModal = this.modalService.open(ModalComponent, { size: "xl" });
+        openedModal.componentInstance.modalBody = modalBody;
+        openedModal.componentInstance.title = title;
+    }
 
     public setSelected(id: string): void {
         this.selectedMemberId = id;
